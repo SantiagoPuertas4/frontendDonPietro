0521990e2e5157df0e5430da4c7323da6f81ddfc
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-08-16 12:26:32 -0300

    creando componentes para login y archivo para vista LoginView

---
+++ src/components/Common/Login/LoginForm.jsx
@@ -0,0 +1,116 @@
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { postLoginFn } from '../../api/auth';
+// import { useSession } from '../../stores/useSession';
+import Input from '../../ui/input/Input';
+import { useSession } from '../../../stores/useSession';
+
+const LoginForm = () => {
+  // ---------------------------------------------
+  // Zustand
+  // ---------------------------------------------
+
+  const { login } = useSession();
+
+  // ---------------------------------------------
+  // RRD
+  // ---------------------------------------------
+
+  const navigate = useNavigate();
+
+  // ---------------------------------------------
+  // RHF
+  // ---------------------------------------------
+
+  const {
+    register,
+    handleSubmit: onSubmitRHF,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  // ---------------------------------------------
+  // RQ
+  // ---------------------------------------------
+
+  const { mutate: postLogin } = useMutation({
+    mutationFn: postLoginFn,
+    onSuccess: (userData) => {
+      toast.dismiss();
+      toast.success(`Bienvenido, ${userData.fullname}`);
+
+      reset();
+
+      // Hacer el login en el cliente
+      login(userData);
+
+      setTimeout(() => {
+        navigate('/');
+      }, 1500);
+    },
+    onError: (e) => {
+      toast.dismiss();
+      toast.warning(e.message);
+    },
+  });
+
+  // ---------------------------------------------
+  // HANDLERS
+  // ---------------------------------------------
+
+  const handleSubmit = (data) => {
+    toast.loading('Cargando...');
+    postLogin(data);
+  };
+
+  // ---------------------------------------------
+  // RENDERIZADO
+  // ---------------------------------------------
+
+  return (
+    <form onSubmit={onSubmitRHF(handleSubmit)}>
+      <Input
+        className='mb-3'
+        error={errors.usernameOrEmail}
+        label='Nombre de usuario o Email'
+        name='usernameOrEmail'
+        options={{
+          required: {
+            value: true,
+            message: 'Este campo es requerido',
+          },
+          minLength: 3,
+          maxLength: 50,
+        }}
+        register={register}
+      />
+      <Input
+        error={errors.password}
+        label='Contraseña'
+        name='password'
+        options={{
+          required: {
+            value: true,
+            message: 'Este campo es requerido',
+          },
+          minLength: 3,
+          maxLength: 20,
+        }}
+        register={register}
+        type='password'
+      />
+      <div className='text-end mt-3'>
+        <button className='btn btn-danger' type='submit'>
+          Ingresar
+        </button>
+      </div>
+      <p className='text-center text-md-start mt-2 mt-lg-0'>
+        ¿Primera vez? <Link to='/register'>Create un usuario acá</Link>
+      </p>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import LoginForm from '../components/Common/Login/LoginForm';
 
 const LoginView = () => {
   return (
-    <div>LoginView</div>
-  )
-}
+    <section className='card bg-white text-dark p-3'>
+      <div className='row g-3'>
+        {/* Parte izquierda */}
+        <article className='col-12 col-md-6'>
+          <h1>Bienvenido</h1>
+          <hr />
+          <LoginForm />
+        </article>
 
+        {/* Parte derecha */}
+        <article className='col-12 col-md-6'>
+          <img
+            alt='Empanadas tucumanas'
+            className='rounded w-100'
+            src='https://www.infotuc.com.ar/wp-content/uploads/2022/02/empanadas-tucumanas-1024x683.jpg'
+          />
+        </article>
+      </div>
+    </section>
+  );
+};
 export default LoginView;
