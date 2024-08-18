b7b1d5ec7812113113585d211a7b4ac2090f0f36
nacho <nachosalpaz0@gmail.com>
2024-08-17 22:39:33 -0300

    creo pagina de footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// import  React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-dark text-secondary">
+      <hr />
+      <section className="container text-center pt-5">
+        <h1>Copyrigth 2024 | DonPietro | Via Chiri 3017</h1>
+        <h2>(381)155-555-555</h2>
+        <div className="d-flex justify-content-evenly py-4 icon-link ">
+          <a href="https://www.facebook.com/" className="icon" target="_blank">
+            <i className="bi bi-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com/" className="icon" target="_blank">
+            <i className="bi bi-instagram "></i>
+          </a>
+        </div>
+      </section>
+    </footer>
+  );
+};
 
 export default Footer;
