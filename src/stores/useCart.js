2789084deb782214bc5e6c2fd70847b9ca7c1f82
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-08-21 02:27:51 -0300

    Modifico useCart.js para no perder el carrito al actualizar

--- src/stores/useCart.js
+++ src/stores/useCart.js
@@ -1,42 +1,55 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
-export const useCart = create((set, get) => ({
-  items: [],
-  addToCart: (product) => {
-    const existingItem = get().items.find(item => item.id === product.id);
+export const useCart = create((set, get) => {
+  const storedItems = sessionStorage.getItem("cartItems");
+  const initialItems = storedItems ? JSON.parse(storedItems) : [];
 
-    if (existingItem) {
-      set((state) => ({
-        items: state.items.map((item) =>
-          item.id === product.id && item.quantity < product.stock
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        ),
-      }));
-    } else {
-      set((state) => ({
-        items: [...state.items, { ...product, quantity: 1 }],
-      }));
-    }
-  },
-  removeItem: (itemId) => {
-    set((state) => ({
-      items: state.items.filter((item) => item.id !== itemId),
-    }));
-  },
-  updateItemQuantity: (itemId, quantity) => {
-    set((state) => ({
-      items: state.items.map((item) =>
-        item.id === itemId
-          ? { ...item, quantity }
-          : item
-      ),
-    }));
-  },
-  clearCart: () => {
-    set({ items: [] });
-  },
-  getCartItem: (itemId) => {
-    return get().items.find(item => item.id === itemId);
-  },
-}));
+  return {
+    items: initialItems,
+    addToCart: (product) => {
+      const existingItem = get().items.find((item) => item.id === product.id);
+
+      set((state) => {
+        const newItems = existingItem
+          ? state.items.map((item) =>
+              item.id === product.id && item.quantity < product.stock
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            )
+          : [...state.items, { ...product, quantity: 1 }];
+
+        sessionStorage.setItem("cartItems", JSON.stringify(newItems));
+
+        return { items: newItems };
+      });
+    },
+    removeItem: (itemId) => {
+      set((state) => {
+        const newItems = state.items.filter((item) => item.id !== itemId);
+
+        sessionStorage.setItem("cartItems", JSON.stringify(newItems));
+
+        return { items: newItems };
+      });
+    },
+    updateItemQuantity: (itemId, quantity) => {
+      set((state) => {
+        const newItems = state.items.map((item) =>
+          item.id === itemId ? { ...item, quantity } : item
+        );
+
+        sessionStorage.setItem("cartItems", JSON.stringify(newItems));
+
+        return { items: newItems };
+      });
+    },
+    clearCart: () => {
+      set({ items: [] });
+
+      sessionStorage.removeItem("cartItems");
+    },
+    getCartItem: (itemId) => {
+      return get().items.find((item) => item.id === itemId);
+    },
+  };
+});
