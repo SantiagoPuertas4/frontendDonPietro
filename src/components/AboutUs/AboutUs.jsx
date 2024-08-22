6d3327c553b3a82f6e5c560f09068eeefc68eeab
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-08-22 16:09:18 -0300

    creando pagina acercaDe con informacion de miembros del grupo

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,52 +1,102 @@
 // AboutUs.jsx
-import './AboutUs.css';
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
-    <section className="about-us">
-      <h1 className="title">Somos Don Pietro</h1>
-      <p className="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      </p>
-      <div className="row m-4">
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card">
-            <img src="./benja-img.jpg" className="card-img-top" alt="Member 1" />
-            <div className="card-body">
-              <h5 className="card-title">Benjamin Gimenez</h5>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sequi delectus deserunt, soluta animi tempore!</p>
+    <>
+      <section>
+        <h1 className="about-header">Acerca de nosotros</h1>
+      </section>
+      <section className="container text-center mt-3">
+        <h2 className="title">Somos Don Pietro</h2>
+        <p className="description ">
+          Somos cuatro amigos con una pasión en común: crear la mejor página
+          para Don Pietro, donde la tecnología se fusiona con la tradición para
+          ofrecer una experiencia única.
+        </p>
+      </section>
+      <section className="about-us">
+        <div className="row m-4">
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="card">
+              <img
+                src="./benja-img.jpg.jpeg"
+                className="card-img-top"
+                alt="Member 1"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Benjamin Gimenez</h5>
+                <p className="card-text">
+                  Mi pasión por el mundo de la tecnología, el diseño y la
+                  informática no solo es una parte integral de mi vida, sino
+                  también una fuerza impulsora en mi carrera profesional. A lo
+                  largo de los años, he demostrado mi habilidad para absorber
+                  rápidamente nuevas tecnologías y conceptos.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="card">
+              <img
+                src="./anabela-img.jpg.jpeg"
+                className="card-img-top"
+                alt="Member 2"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Anabela Guillermo</h5>
+                <p className="card-text">
+                  Soy una apasionada del aprendizaje con un máster en Marketing
+                  Digital, certificada como Fotógrafa, con formación en Tester
+                  QA Manual y Programación Full Stack. La música es una parte
+                  esencial de mi vida; canto y toco guitarra, piano, y
+                  violonchelo. Siempre busco nuevas oportunidades para expandir
+                  mis conocimientos y habilidades.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="card">
+              <img
+                src="./santi-img.jpg.jpeg"
+                className="card-img-top"
+                alt="Member 3"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Santiago Puertas</h5>
+                <p className="card-text">
+                  Apasionado por el conocimiento y los misterios del cosmos, me
+                  considero un eterno estudiante del universo y sus leyes. La
+                  física no es solo una ciencia para mí, sino una ventana a los
+                  secretos más profundos de la realidad. Además, la programación
+                  y la resolución de problemas lógicos son el terreno donde
+                  ejercito mi mente, enfrentándome a desafíos que afinan mi
+                  ingenio.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="card">
+              <img src="/member4.jpg" className="card-img-top" alt="Member 4" />
+              <div className="card-body">
+                <h5 className="card-title">Ignacio sal paz</h5>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Similique vero, officia dolores soluta excepturi non in illum
+                  error impedit iure pariatur eos ipsum voluptate tenetur
+                  recusandae quae, deleniti rerum et accusantium dolorem
+                  molestias aut? Reiciendis, nam itaque vitae quasi dolorum
+                  culpa quibusdam et dolores explicabo facilis cum velit beatae
+                  enim?
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card">
-            <img src="/member2.jpg" className="card-img-top" alt="Member 2" />
-            <div className="card-body">
-              <h5 className="card-title">Anabela Guillermo</h5>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi nisi magni recusandae eos aliquam. Voluptate!</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card">
-            <img src="/member3.jpg" className="card-img-top" alt="Member 3" />
-            <div className="card-body">
-              <h5 className="card-title">Santiago Puertas</h5>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloremque quidem quasi, vero cum delectus.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-md-6 col-lg-3 mb-4">
-          <div className="card">
-            <img src="/member4.jpg" className="card-img-top" alt="Member 4" />
-            <div className="card-body">
-              <h5 className="card-title">Ignacio sal paz</h5>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laudantium inventore, dicta obcaecati vero id?</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
